56c679942ddc0594d9ff6749c1699d5d3ea345ea
TANVIR KAUR <tanvirkaur1804@gmail.com>
2021-09-30 22:01:44 -0400

    initialized the salary claculation project using react

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-danger">
+        <h1>This is testing bootstrap</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
